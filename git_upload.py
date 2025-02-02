1bc7284b7df6459f4d5f51427f79fd061be8e8bc
ahnsam207 <2ok25@daum.net>
2025-02-02 19:18:51 +0900

    Update git_upload.py

--- git_upload.py
+++ git_upload.py
@@ -31,6 +31,6 @@ if uploaded_file is not None:
     response = requests.put(url, json=data, headers={"Authorization": f"token {GITHUB_TOKEN}"})
 
     if response.status_code == 201:
-        st.success(f"✅ 계획서 파일이 정상적으로 업로드되었습니다: {file_path}")
+        st.success("✅ 계획서 파일이 정상적으로 업로드되었습니다.")
     else:
         st.error("❌ 업로드 실패(파일의 이름을 수정해 주세요.)")
